895ea2b72dbecf49949ef196dbced28f1549ca73
bcottman <you@egmail.com>
2021-02-15 21:01:08 +0000

    sandbox commit that passesd all local unit tests

---
+++ tests/test_src.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+
+# import pytest
+
+
+# def test_fail():
+#     # test  fail"
+#     assert False
+
+
+def test_1():
+    """Sample pytest test function with the pytest fixture as an argument."""
+    assert True
+
+
+def test_2():
+    """Sample pytest test function with the pytest fixture as an argument."""
+    assert True
+
+
+# def test_2(response):
+#     """Sample pytest test function with the pytest fixture as an argument."""
+#     assert response(0)
+#
+#
+# def test_3(response):
+#     """Sample pytest test function with the pytest fixture as an argument."""
+#     assert response(1)
